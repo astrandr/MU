125d357add1d8da1e65c929f0d92d60bdec19de5
astrandr <andreyb2001@yahoo.com>
2026-06-27 08:52:43 +0300

    Adding tests

---
+++ UnitTestMSimple/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestMSimple")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestMSimple")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae4b2ca0-3bd8-489c-a197-bb2b48f85dda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestMSimple/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MUtils;
+
+namespace MUtils.Tests
+{
+    [TestClass]
+    public class MSimpleTests
+    {
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_ForPositiveNumbers()
+        {
+            var simple = new MSimple();
+            double result = simple.Add(2.5, 3.5);
+            Assert.AreEqual(6.0, result, 1e-10);
+        }
+
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_ForNegativeNumbers()
+        {
+            var simple = new MSimple();
+            double result = simple.Add(-2.0, -3.0);
+            Assert.AreEqual(-5.0, result, 1e-10);
+        }
+
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_ForMixedNumbers()
+        {
+            var simple = new MSimple();
+            double result = simple.Add(-2.0, 3.0);
+            Assert.AreEqual(1.0, result, 1e-10);
+        }
+
+        [TestMethod]
+        public void Add_ReturnsCorrectSum_WhenAddingZero()
+        {
+            var simple = new MSimple();
+            double result = simple.Add(0.0, 5.0);
+            Assert.AreEqual(5.0, result, 1e-10);
+        }
+    }
+}
